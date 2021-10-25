e9156ff32965c40863e296473f0c4aaa65693e1a
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-25 08:13:45 -0700

    added chart(not showing on results page?) and changed minimal styling to page

--- results/results.js
+++ results/results.js
@@ -23,7 +23,51 @@ for (let item of pokedex){
     capturedSpan.textContent = `Captured: ${item.captured}`;
 
     p.append(encounteredSpan, capturedSpan);
-    div.append( img, p);
+    div.append(img, p);
     container.append(header, div);
     main.append(container);
 }
+
+const names = pokedex.map((item)=>{
+    const poke = findById(item.id, pokemon);
+    return poke.pokemon;
+});
+
+const captured = pokedex.map(item=>item.captured);
+
+var resultsChart = document.getElementById('pokedex-chart').getContext('2d');
+// eslint-disable-next-line no-undef
+new Chart(resultsChart, {
+    type: 'bar',
+    data: {
+        labels: names,
+        datasets: [{
+            label: '# of Times Captured',
+            data: captured,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
